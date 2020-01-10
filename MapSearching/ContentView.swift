0fca9dd03058b1b7fd32695c5727fee815f7d9e1
Alexander Römer <alex@roemer.design>
2020-01-10 19:17:29 +0100

    Initial Commit

---
+++ MapSearching/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapSearching
+//
+//  Created by Alexander Römer on 10.01.20.
+//  Copyright © 2020 Alexander Römer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
